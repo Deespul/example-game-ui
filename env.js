d91176b58365e39953c7bb23c34845ed94a4de6a
modmoto <simonheiss87@gmail.com>
2021-05-31 17:20:38 +0200

    lel

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 const config = {
-  API_URL: "http://localhost:5000",
-  // API_URL: "https://example.backend.matchbox.gg"
-  MATCHBOX_API: "http://localhost:5000",
-  // MATCHBOX_API: "https://matchbox.test.w3champions.com",
+  // API_URL: "http://localhost:5000",
+  API_URL: "https://example.backend.matchbox.gg",
+  // MATCHBOX_API: "http://localhost:5000",
+  MATCHBOX_API: "http://api.test.matchbox.gg/",
 };
