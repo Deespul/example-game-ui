eafd7a8acf1ebe8927256f14bf21bb080f6f5d10
modmoto <simonheiss87@gmail.com>
2021-06-08 14:53:37 +0200

    fixed urls 2

--- public/env.js
+++ public/env.js
@@ -1,6 +1,6 @@
 window._env_ = {
   // API_URL: "http://localhost:5002/",
-  API_URL: "https://demobackend.test.matchbox.gg/",
+  API_URL: "https://demo-backend.test.matchbox.gg/",
   // MATCHBOX_API: "http://localhost:5000/",
   MATCHBOX_API: "https://api.test.matchbox.gg/",
 };
